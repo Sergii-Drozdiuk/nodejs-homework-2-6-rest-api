365191f5a68c0cd4c90eb7e4878bd4139ee3e114
Sergii-Drozdiuk <drozduyk@gmail.com>
2023-12-17 07:36:00 +0200

    hw-2-done

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,51 @@
-// const fs = require('fs/promises')
+import fs from 'fs/promises';
+import path from 'path';
+import { nanoid } from 'nanoid';
 
-const listContacts = async () => {}
+const contactsPath = path.resolve('models', 'contacts.json');
 
-const getContactById = async (contactId) => {}
+const updateContacts = contacts =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-const removeContact = async (contactId) => {}
+export const listContacts = async () => {
+  const date = await fs.readFile(contactsPath);
+  return JSON.parse(date);
+};
 
-const addContact = async (body) => {}
+export const getById = async contactId => {
+  const contacts = await listContacts();
+  const contactById = contacts.find(({ id }) => id === contactId);
+  return contactById || null;
+};
 
-const updateContact = async (contactId, body) => {}
+export const removeContact = async contactId => {
+  const contacts = await listContacts();
+  const removeIndex = contacts.findIndex(({ id }) => id === contactId);
+  if (removeIndex === -1) {
+    return null;
+  }
+  const removeContact = contacts.splice(removeIndex, 1);
+  await updateContacts(contacts);
+  return removeContact;
+};
 
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+export const addContact = async ({ name, email, phone }) => {
+  const newContact = { id: nanoid(), name, email, phone };
+  const contacts = await listContacts();
+  contacts.push(newContact);
+  await updateContacts(contacts);
+  return newContact;
+};
+
+export const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(({ id }) => id === contactId);
+  if (index === -1) {
+    return null;
+  }
+
+  contacts[index] = { ...contacts[index], ...body };
+
+  await updateContacts(contacts);
+  return contacts[index];
+};
